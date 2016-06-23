bc16c1cd827e794145d8b7bd51a4ff22e2917227
RajicDenis <denis.rajic92@gmail.com>
2016-06-23 19:02:55 +0200

    Landing page project

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,400italic' rel='stylesheet' type='text/css'>
+    <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" >-->
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>    
+
+</head>
+
+<body>
+    
+    <div class="body-wrapper">
+
+        <section class="section-about">
+          
+            <nav class="home-nav">
+                
+                <h1 class="logo">Super<span>Mega</span>Awesome</h1>
+
+                <ul class="nav-list">
+                    
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a class="button" href="#">Get in touch</a></li>
+
+                </ul>
+
+            </nav>
+
+            <div class="home-intro">
+
+                <h1 class="home-lead">We create beautiful products.</h1>
+
+                <h2>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</h2>
+
+                <div class="button-wrap">
+                    <a class="button learn-more" href="#">Learn More</a>
+                    <a class="button play-videos" href="#">Play Videos</a>
+                </div>
+            </div>
+
+        </section>  
+
+    </div>
+</body>
+
+</html>
